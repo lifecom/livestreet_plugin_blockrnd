b8dc863ca6bdd1e30a17d689e756bbe3336d1d4b
Mykola <lavirozp@gmail.com>
2016-02-11 19:39:26 +0000

    fixes

--- config/config.php
+++ config/config.php
@@ -4,7 +4,7 @@ $config = array();
 
 $config['topic_last_days_count'] = 5;  	// За какой период выводить топики?
 $config['topic_count'] = 6;  			// Сколько записей показывать в блоке?
-$config['topic_from_blogs'] = [1,2,3,4]; 	// id блогов из которых выводить топики
+$config['topic_from_blogs'] = array(1,2,3,4); 	// id блогов из которых выводить топики
 
 
 // Настройки вывода блока
